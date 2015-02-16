1147efcdabc0f6029707138fc221c6a09e61484e
greye <greye@null.net>
2015-02-16 19:32:19 +0300

    add missing DependencyDatabasePlugin.h

---
+++ src/DependencyDatabasePlugin.h
@@ -0,0 +1,11 @@
+#ifndef CLANG_RENAME_DEPENDENCYDB_PLUGIN_H
+#define CLANG_RENAME_DEPENDENCYDB_PLUGIN_H
+
+namespace clang {
+namespace rename {
+void
+registerDependencyDatabasePlugin();
+}
+}
+
+#endif
